75a9317f74663f749e0db50868db9c87f9cde8aa
Lisa Campbell <campb215@purdue.edu>
2016-01-17 01:00:32 -0500

    Added google sign in

---
+++ login.js
@@ -0,0 +1,13 @@
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+}
+//gets profile
+
+function onSignIn(googleUser) {
+  var id_token = googleUser.getAuthResponse().id_token;
+}
+//gets id token
